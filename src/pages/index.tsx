f421e0c14c44a264bb30f6b3f64321c3a321e6bb
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-19 19:49:50 -0300

    Add Container as default at Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import BannerSection from 'components/BannerSection'
+import { Container } from 'components/Container'
+import ContentSection from 'components/ContentSection'
 import Navbar from 'components/Navbar'
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <BannerSection />
+      <Container>
+        <BannerSection />
+        <ContentSection />
+      </Container>
     </>
   )
 }
