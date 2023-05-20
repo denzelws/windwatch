1603bc0fb03d38f1351840a096362248bc958c88
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-20 01:50:22 -0300

    Add axios and weatherApi

---
+++ src/utils/weatherApi.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import { WeatherData } from 'hooks/useWeatherDataList'
+
+const getWeatherData = async (cityName: string) => {
+  const apiKey = 'ea4023385742a12398dfda46caa285da'
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+
+  try {
+    const response = await axios.get(apiUrl)
+    return transformData(response.data)
+  } catch (error) {
+    console.error('Error fetching weather data:', error)
+    throw error
+  }
+}
+
+const transformData = (data: WeatherData) => {
+  const kelvinToCelsius = (kelvin: number) => {
+    const celsius = kelvin - 273.15
+    return celsius.toFixed(2)
+  }
+
+  const celsiusTemp = kelvinToCelsius(data.main.temp)
+  return {
+    name: data.name,
+    main: {
+      temp: Number(celsiusTemp)
+    },
+    weather: [
+      {
+        description: data.weather[0].description
+      }
+    ]
+  }
+}
+
+export { getWeatherData }
