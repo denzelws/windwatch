060cdd70c967d6fa1e8a840ce76adf8f41b1641f
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-20 01:48:55 -0300

    Add custom hook to ContentSection

--- src/components/ContentSection/index.tsx
+++ src/components/ContentSection/index.tsx
@@ -1,50 +1,44 @@
 import Button from 'components/Button'
 import * as S from './styles'
+import useWeatherDataList from 'hooks/useWeatherDataList'
 
-import { Search } from '@styled-icons/ionicons-outline/Search'
+const ContentSection = () => {
+  const cities = ['New York', 'London', 'Canada']
+  const weatherDataList = useWeatherDataList(cities)
 
-export type ContentSectionProps = {
-  title: string
-  temperature: string
-  status: string
+  return (
+    <S.ContentSection>
+      <S.InputWrapper>
+        <S.InputBox>
+          <S.SearchIcon size={22} />
+          <S.Input type="text" placeholder="Enter your city..." />
+          <Button backgroundColor="darkpurple" size="small">
+            Search
+          </Button>
+        </S.InputBox>
+      </S.InputWrapper>
+
+      <S.InformationWrapper>
+        <S.TitleWrapper>
+          <S.Title>Recent Place</S.Title>
+        </S.TitleWrapper>
+
+        <S.InformationBox>
+          {weatherDataList.map((weatherData, index) => (
+            <S.Box key={index}>
+              {weatherData && (
+                <>
+                  <S.City>{weatherData.name}</S.City>
+                  <S.Temperature>{weatherData.main.temp}</S.Temperature>
+                  <S.Status>{weatherData.weather[0].description}</S.Status>
+                </>
+              )}
+            </S.Box>
+          ))}
+        </S.InformationBox>
+      </S.InformationWrapper>
+    </S.ContentSection>
+  )
 }
-
-const ContentSection = () => (
-  <S.ContentSection>
-    <S.InputWrapper>
-      <S.InputBox>
-        <Search size={22} />
-        <S.Input type="text" placeholder="Enter your city..." />
-        <Button backgroundColor="darkpurple" size="small">
-          Search
-        </Button>
-      </S.InputBox>
-    </S.InputWrapper>
-
-    <S.InformationWrapper>
-      <S.TitleWrapper>
-        <S.Title>Recent Place</S.Title>
-      </S.TitleWrapper>
-
-      <S.InformationBox>
-        <S.Box>
-          <S.City>New York</S.City>
-          <S.Temperature>12</S.Temperature>
-          <S.Status>Raining</S.Status>
-        </S.Box>
-        <S.Box>
-          <S.City>New York</S.City>
-          <S.Temperature>12</S.Temperature>
-          <S.Status>Raining</S.Status>
-        </S.Box>
-        <S.Box>
-          <S.City>New York</S.City>
-          <S.Temperature>12</S.Temperature>
-          <S.Status>Raining</S.Status>
-        </S.Box>
-      </S.InformationBox>
-    </S.InformationWrapper>
-  </S.ContentSection>
-)
 
 export default ContentSection
