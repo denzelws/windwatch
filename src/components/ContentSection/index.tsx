48cd9bda6cf1e2f5f40cd825db5d885505c096cb
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-05-23 16:52:21 -0300

    Add initial logic to handle input and form

--- src/components/ContentSection/index.tsx
+++ src/components/ContentSection/index.tsx
@@ -1,20 +1,43 @@
 import Button from 'components/Button'
 import * as S from './styles'
 import useWeatherDataList from 'hooks/useWeatherDataList'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 const ContentSection = () => {
   const cities = ['New York', 'London', 'Canada']
+  const [text, setText] = useState('')
   const weatherDataList = useWeatherDataList(cities)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value
+    setText(inputValue)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('Submitted:', text)
+  }
+
+  const handleIconSubmit = () => {
+    console.log('Icon Submitted:', text)
+  }
 
   return (
     <S.ContentSection>
       <S.InputWrapper>
         <S.InputBox>
-          <S.SearchIcon size={22} />
-          <S.Input type="text" placeholder="Enter your city..." />
-          <Button backgroundColor="darkpurple" size="small">
-            Search
-          </Button>
+          <form onSubmit={handleSubmit}>
+            <S.SearchIcon size={22} onClick={handleIconSubmit} />
+            <S.Input
+              type="text"
+              placeholder="Enter your city..."
+              value={text}
+              onChange={handleChange}
+            />
+            <Button backgroundColor="darkpurple" size="small" type="submit">
+              Search
+            </Button>
+          </form>
         </S.InputBox>
       </S.InputWrapper>
 
